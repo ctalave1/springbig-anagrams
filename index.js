c6b9e22fa4cea27d6d18c8429a22d1be23fc4928
Christopher Talavera <christopher.talavera@smartbear.com>
2021-08-31 19:22:50 -0400

    Adding .env file to root of project with API_PORT env variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 const server = http.createServer(app);
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 5000;
 
 server.listen(port, () => {
   console.log(`springbig-anagrams api running on port ${port}`);
